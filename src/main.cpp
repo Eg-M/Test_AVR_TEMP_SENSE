42542f6f2dc56b1877ca2853361ff7aababaaf85
Eg-M <musaev.egit@gmail.com>
2021-06-21 16:43:25 +0200

    version_for_heater

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,8 @@
 #include <Arduino.h>
-
-/*
-    basic_demo.ino
-    Example for MCP9600
-    Copyright (c) 2018 Seeed Technology Co., Ltd.
-    Website    : www.seeed.cc
-    Author     : downey
-    Create Time: May 2018
-    Change Log :
-    The MIT License (MIT)
-    Permission is hereby granted, free of charge, to any person obtaining a copy
-    of this software and associated documentation files (the "Software"), to deal
-    in the Software without restriction, including without limitation the rights
-    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-    copies of the Software, and to permit persons to whom the Software is
-    furnished to do so, subject to the following conditions:
-    The above copyright notice and this permission notice shall be included in
-    all copies or substantial portions of the Software.
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-    THE SOFTWARE.
-*/
-
 #include "Seeed_SHT35.h"
   
      
-/*SAMD core*/ 
+/*SAMD core*/
 #ifdef ARDUINO_SAMD_VARIANT_COMPLIANCE
     #define SDAPIN  20
     #define SCLPIN  21
@@ -38,46 +11,60 @@
 #else
     #define SDAPIN  A4
     #define SCLPIN  A5
-    //#define RSTPIN  4
+ 
     #define SERIAL Serial
 #endif
+#define TEMP1 2
+#define TEMP2 4
+
+bool flag = 1;
+float temp1, hum1;
+float temp2, hum2;
+
 
 SHT35 sensor(SCLPIN);
 
 
 void setup() {
-    SERIAL.begin(115200);
+    SERIAL.begin(9600);
     delay(10);
     SERIAL.println("serial start!!");
     if (sensor.init()) {
         SERIAL.println("sensor init failed!!!");
     }
+    pinMode(TEMP1,OUTPUT);
+    pinMode(TEMP2,OUTPUT);
+    digitalWrite(TEMP1,flag);
     delay(1000);
 }
 
 
 void loop() {
-    u16 value = 0;
-    u8 data[6] = {0};
-    float temp, hum;
-    if (NO_ERROR != sensor.read_meas_data_single_shot(HIGH_REP_WITH_STRCH, &temp, &hum)) {
-        SERIAL.println("read temp failed!!");
-        SERIAL.println("   ");
-        SERIAL.println("   ");
-        SERIAL.println("   ");
-    } else {
-        SERIAL.println("read data :");
-        SERIAL.print("temperature = ");
-        SERIAL.print(temp);
-        SERIAL.println(" ℃ ");
-
-        SERIAL.print("humidity = ");
-        SERIAL.print(hum);
-        SERIAL.println(" % ");
-
-        SERIAL.println("   ");
-        SERIAL.println("   ");
-        SERIAL.println("   ");
+  
+    if (!(millis() % 1000))
+    {
+        
+        
+        if (flag)
+        {
+            
+            if (NO_ERROR != sensor.read_meas_data_single_shot(HIGH_REP_WITH_STRCH, &temp1, &hum1))
+            {
+                SERIAL.println("read temp1 failed!!");
+                SERIAL.println("   ");
+                SERIAL.println("   ");
+                SERIAL.println("   ");
+            }
+           
+        }
+        
+        SERIAL.print(temp1);
+        SERIAL.print(',');
+        SERIAL.println(hum1);
+       
     }
-    delay(1000);
+    if (millis() % 5000)
+        sensor.change_heater_status(CMD_HEATER_OFF);
+        else sensor.change_heater_status(CMD_HEATER_ON);
+  
 }
